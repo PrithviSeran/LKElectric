f5a3037203565a766a6d9297416e6881def240ad
prithviseran <prithisha33@gmail.com>
2026-04-28 12:56:56 -0400

    email

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const PHONE_TEL = 'tel:+14165051115'
 export const PHONE_DISPLAY = '416-505-1115'
 
 /** Receives submissions from the site contact form (FormSubmit relay) */
-export const CONTACT_EMAIL = 'armaanmakowal10@gmail.com' as const
+export const CONTACT_EMAIL = 'lkelectricservicestoronto@gmail.com' as const
